ad1b4afecd2981692388da9d479b6aadd6ae7ff5
Lifailon <deadepicboss@gmail.com>
2024-12-14 00:16:37 +0300

    debug decode cyrillic

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -32,8 +32,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const requestHeaders: Record<string, string> = {
         'Content-Type': Array.isArray(headers['content-type']) ? headers['content-type'][0] : headers['content-type'] || 'application/x-www-form-urlencoded',
         'accept': Array.isArray(headers['accept']) ? headers['accept'][0] : headers['accept'] || '',
-        'accept-language': Array.isArray(headers['accept-language']) ? headers['accept-language'][0] : headers['accept-language'] || 'ru,ru-RU;q=0.9,en;q=0.8',
-        'accept-charset': 'utf-8, windows-1251;q=0.9',
+        'accept-language': Array.isArray(headers['accept-language']) ? headers['accept-language'][0] : headers['accept-language'] || '',
+        'accept-charset': '',
         'accept-encoding': Array.isArray(headers['accept-encoding']) ? headers['accept-encoding'][0] : headers['accept-encoding'] || '',
         'cookie': Array.isArray(headers['cookie']) ? headers['cookie'][0] : headers['cookie'] || '',
         'cache-control': Array.isArray(headers['cache-control']) ? headers['cache-control'][0] : headers['cache-control'] || '',
