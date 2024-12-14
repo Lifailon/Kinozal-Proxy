ed9992a794f1d41fc0df2049e336b346c20ac658
Lifailon <deadepicboss@gmail.com>
2024-12-14 18:34:02 +0300

    debug decode cyrillic

--- middleware.ts
+++ middleware.ts
@@ -8,22 +8,28 @@ export function middleware(req: NextRequest) {
 
     if (url.searchParams.has('s')) {
         const searchParam = url.searchParams.get('s')
-        console.log('Original search param:', searchParam)
 
-        try {
-            // Декодируем как Windows-1251
-            const buffer = Buffer.from(searchParam, 'hex')
-            const decodedSearch = iconv.decode(buffer, 'windows-1251')
+        // Проверяем, что searchParam не null
+        if (searchParam) {
+            console.log('Original search param:', searchParam)
 
-            // Перекодируем в UTF-8 и затем в URL-encoded
-            const encodedSearch = encodeURIComponent(decodedSearch)
+            try {
+                // Декодируем как Windows-1251
+                const buffer = Buffer.from(searchParam, 'utf-8') // Преобразуем строку в буфер
+                const decodedSearch = iconv.decode(buffer, 'windows-1251')
 
-            console.log('Decoded search param (UTF-8):', decodedSearch)
-            console.log('Encoded search param (URL-encoded):', encodedSearch)
+                // Перекодируем в UTF-8 и затем в URL-encoded
+                const encodedSearch = encodeURIComponent(decodedSearch)
 
-            url.searchParams.set('s', encodedSearch)
-        } catch (error) {
-            console.error('Error during decoding:', error)
+                console.log('Decoded search param (UTF-8):', decodedSearch)
+                console.log('Encoded search param (URL-encoded):', encodedSearch)
+
+                url.searchParams.set('s', encodedSearch)
+            } catch (error) {
+                console.error('Error during decoding:', error)
+            }
+        } else {
+            console.warn('Search param "s" is null or empty.')
         }
     }
 
